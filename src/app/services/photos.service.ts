70c4f9abf5a01d11b85139d2a20f04d874bd4c5c
matheusjordan <matheusj1599@outlook.com>
2024-11-03 11:47:27 -0300

    Create save photo to web and mobile platforms

--- src/app/services/photos.service.ts
+++ src/app/services/photos.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera';
 import {UserPhoto} from "../models/user-photo.model";
+import {Directory, Filesystem} from "@capacitor/filesystem";
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +16,8 @@ export class PhotosService {
 
     if (hasPermission.photos === 'granted') {
       const capturedPhoto = await this.takePhoto();
-
-      this.photos.unshift({
-        filePath: 'processing...',
-        webviewPath: capturedPhoto.webPath!
-      })
+      const savedPhoto = await this.savePhoto(capturedPhoto);
+      this.photos.unshift(savedPhoto);
     } else {
       await Camera.requestPermissions();
     }
@@ -32,4 +30,37 @@ export class PhotosService {
       quality: 100
     });
   }
+
+  private async savePhoto(photo: Photo): Promise<UserPhoto> {
+    const filename = `${Date.now()}.jpeg`;
+
+    const savedFile = await Filesystem.writeFile({
+      path: filename,
+      data: await this.readAsBase64(photo),
+      directory: Directory.Data
+    });
+
+    return {
+      filePath: filename,
+      webviewPath: photo.webPath
+    }
+  }
+
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+
+    return await this.convertBlobAsBase64(blob) as string;
+  }
+
+  private convertBlobAsBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    }
+
+    reader.readAsDataURL(blob);
+  })
 }
